cf213fdaf76ac162a250bd2f5a01c53c04edda7f
Sk13-Satyam <cloudsk13new@gmail.com>
2025-12-06 10:55:49 +0530

    fixed payment issue

--- frontend/src/components/OrderSummary.jsx
+++ frontend/src/components/OrderSummary.jsx
@@ -6,7 +6,7 @@ import { loadStripe } from "@stripe/stripe-js";
 import axios from "../lib/axios";
 
 const stripePromise = loadStripe(
-	"pk_test_51KZYccCoOZF2UhtOwdXQl3vcizup20zqKqT9hVUIsVzsdBrhqbUI2fE0ZdEVLdZfeHjeyFXtqaNsyCJCmZWnjNZa00PzMAjlcL"
+	"pk_test_51SbC1H0uWF2QiKoFNTp3PsmmWvrT2FYw6GLF5YQrkvVcB8I6Iq8KYOPqe7Tex5SI33x4Hmfxi9wytg8IZwiqPjTW005UyEI1F2"
 );
 
 const OrderSummary = () => {
