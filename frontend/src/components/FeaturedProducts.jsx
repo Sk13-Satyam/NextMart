8967c1a9fcb37b6a091e4c9914e110c41a981c25
Sk13-Satyam <cloudsk13new@gmail.com>
2025-12-08 10:56:28 +0530

    fixed typo in FeaturedProducts component

--- frontend/src/components/FeaturedProducts.jsx
+++ frontend/src/components/FeaturedProducts.jsx
@@ -71,7 +71,7 @@ const FeaturedProducts = ({ featuredProducts }) => {
 												${product.price.toFixed(2)}
 											</p>
 											<button
-												onClick={() => addToCart(product)}
+												onClick={() => handleAddToCart(product)}
 												className='w-full bg-emerald-600 hover:bg-emerald-500 text-white font-semibold py-2 px-4 rounded transition-colors duration-300 
 												flex items-center justify-center'
 											>
